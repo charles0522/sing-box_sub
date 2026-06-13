9e726258a02c9c7b8cf44f8d7cd0c76885c557b8
charles0522 <162699321+charles0522@users.noreply.github.com>
2026-06-14 02:16:51 +0800

    Update update-sub.sh

--- update-sub.sh
+++ update-sub.sh
@@ -13,10 +13,11 @@ docker build -t sing-box-subscribe:latest .
 echo "========= 2. 检查并清理旧容器 ========="
 # 定义要检查的容器关键字
 CONTAINER_NAME="sing-box-subscribe"
-
+CONTAINER_IDS="docker ps -a | grep "$CONTAINER_NAME" | awk '{print $1}'"
 if [ -n "$CONTAINER_IDS" ]; then
-    echo "发现正在运行或残留的容器，正在删除..."
-docker ps -a | grep "$CONTAINER_NAME" | awk '{print $1}' | xargs  docker rm -f
+    echo "发现正在运行或残留的容器，正在停止并删除..."
+    docker stop "$CONTAINER_IDS"
+    docker rm -f "$CONTAINER_IDS"
     echo "旧容器清理完毕。"
 else
     echo "未发现相关的旧容器，跳过清理步骤。"
